c15ee704fc6abb8575a036516e39154367f77ca9
KilianL55 <79110244+KilianL55@users.noreply.github.com>
2022-10-19 09:20:55 +0200

    add initial project

---
+++ src/test/java/edu/caensup/sio/emusic/EMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.caensup.sio.emusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
