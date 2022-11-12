b6bd7a106cea72a20c8f9a4b474578cdc7e1d35d
KilianL55 <79110244+KilianL55@users.noreply.github.com>
2022-11-12 16:35:44 +0100

    remove children

--- src/main/java/edu/caensup/sio/emusic/Controller/UserController.java
+++ src/main/java/edu/caensup/sio/emusic/Controller/UserController.java
@@ -16,10 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.mail.MessagingException;
@@ -164,6 +161,12 @@ public class UserController {
         vue.addData("active","disable");
         repoEnfant.save(enfant);
         return new RedirectView("dashboard");
+    }
+
+    @RequestMapping("removeChildren/{id}")
+    public RedirectView removeChildren(@PathVariable int id){
+        repoEnfant.deleteById(id);
+        return new RedirectView("/dashboard");
     }
 
     @GetMapping("removeAccount")
