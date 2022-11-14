34a8f7351ec86c2f71f598f43ad29deed186975b
KilianL55 <79110244+KilianL55@users.noreply.github.com>
2022-11-14 08:29:18 +0100

    Update EMusicApplication.java

--- src/main/java/edu/caensup/sio/emusic/EMusicApplication.java
+++ src/main/java/edu/caensup/sio/emusic/EMusicApplication.java
@@ -2,12 +2,19 @@ package edu.caensup.sio.emusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EMusicApplication {
+public class EMusicApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EMusicApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EMusicApplication.class);
     }
 
 }
