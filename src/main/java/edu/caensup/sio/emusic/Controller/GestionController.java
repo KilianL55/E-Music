b137e52111f973d1ca48c437aa42e76f142ad20f
jcheron <myaddressmail@gmail.com>
2022-11-14 20:16:21 +0100

    add form tester

--- src/main/java/edu/caensup/sio/emusic/Controller/GestionController.java
+++ src/main/java/edu/caensup/sio/emusic/Controller/GestionController.java
@@ -2,6 +2,8 @@ package edu.caensup.sio.emusic.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,5 +17,15 @@ public class GestionController {
   @RequestMapping("/403")
   public @ResponseBody String accessDeniedAction() {
     return "no access for user";
+  }
+
+  @GetMapping("/form")
+  public String formAction() {
+    return "/formView";
+  }
+
+  @PostMapping("/formSubmit")
+  public @ResponseBody String formSubmitAction(@ModelAttribute String name) {
+    return name;
   }
 }
