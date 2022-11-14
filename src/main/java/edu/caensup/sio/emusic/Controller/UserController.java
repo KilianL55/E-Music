4d7e3ff0d8e65dfac2ae53a519ca807268ce36b7
KilianL55 <79110244+KilianL55@users.noreply.github.com>
2022-11-14 11:17:44 +0100

    Update UserController.java

--- src/main/java/edu/caensup/sio/emusic/Controller/UserController.java
+++ src/main/java/edu/caensup/sio/emusic/Controller/UserController.java
@@ -151,7 +151,7 @@ public class UserController {
     }
 
     @PostMapping("saveChildren")
-    public RedirectView saveChildren(@ModelAttribute Enfant enfant){
+    public @ResponseBody Enfant saveChildren(@ModelAttribute Enfant enfant){
         Responsable resp = repoResponsable.findByUsername(enfant.getEmail_parent());
         enfant.setUsername(enfant.getPrenom()+"."+enfant.getNom());
         enfant.setPassword(passwordEncoder.encode(enfant.getPassword()));
@@ -160,7 +160,7 @@ public class UserController {
         vue.addData("isActive","children");
         vue.addData("active","disable");
         repoEnfant.save(enfant);
-        return new RedirectView("dashboard");
+        return enfant;
     }
 
     @RequestMapping("removeChildren/{id}")
