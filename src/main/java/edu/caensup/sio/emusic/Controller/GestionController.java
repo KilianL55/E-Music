acac785cb53d4e79b6f2e1ea97a328e29f249de3
jcheron <myaddressmail@gmail.com>
2022-11-14 19:58:11 +0100

    add 403 url

--- src/main/java/edu/caensup/sio/emusic/Controller/GestionController.java
+++ src/main/java/edu/caensup/sio/emusic/Controller/GestionController.java
@@ -2,11 +2,18 @@ package edu.caensup.sio.emusic.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GestionController {
-    @GetMapping("/admin")
-    public String indexAction() {
-        return "index";
-    }
+  @GetMapping("/admin")
+  public String indexAction() {
+    return "index";
+  }
+
+  @RequestMapping("/403")
+  public @ResponseBody String accessDeniedAction() {
+    return "no access for user";
+  }
 }
