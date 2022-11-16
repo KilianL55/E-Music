bbd90200ac039e571e7c9b7dcbd29457a0978ea5
KilianL55 <79110244+KilianL55@users.noreply.github.com>
2022-11-16 08:41:28 +0100

    Update CustomErrorController.java

--- src/main/java/edu/caensup/sio/emusic/Controller/CustomErrorController.java
+++ src/main/java/edu/caensup/sio/emusic/Controller/CustomErrorController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
 
-  @RequestMapping("/error")
-  public String handleError() {
-    return "/errorView";
-  }
+//  @RequestMapping("/error")
+//  public String handleError() {
+//    return "/errorView";
+//  }
 }
