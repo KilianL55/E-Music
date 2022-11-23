f6373ead03042c9597ec3ca3d1a220a04b5db89b
Xeltax <85458421+Xeltax@users.noreply.github.com>
2022-11-23 11:37:37 +0100

    Update
    Fix of the pojo ! menu is now dynamic if user is connected

--- src/main/java/edu/caensup/sio/emusic/pojo/ActiveUser.java
+++ src/main/java/edu/caensup/sio/emusic/pojo/ActiveUser.java
@@ -5,21 +5,10 @@ import org.springframework.security.core.Authentication;
 import lombok.Data;
 import lombok.NonNull;
 
+@Data
 public class ActiveUser {
     @NonNull
     private Authentication authentication;
-
-    public ActiveUser(@NonNull Authentication authentication) {
-        this.authentication = authentication;
-    }
-
-    public Authentication getAuthentication() {
-        return authentication;
-    }
-
-    public void setAuthentication(Authentication authentication) {
-        this.authentication = authentication;
-    }
 
     public boolean isConnected() {
         return !(authentication instanceof AnonymousAuthenticationToken);
